853716f155a3ee92e96b801159705e148ceb725c
Kay Alvaer <mosarwe@yahoo.co.uk>
2022-05-30 20:55:27 +0200

    page routing

---
+++ kutek-interns-fo/pages/developers/add.js
@@ -0,0 +1,7 @@
+export default function addDeveloperPage() {
+  return (
+    <div>
+        <h1>Add Developer</h1>
+    </div>
+  )
+}
---
+++ kutek-interns-fo/pages/developers/index.js
@@ -0,0 +1,7 @@
+export default function DeveloperListPage() {
+  return (
+    <div>
+        <h1>Developer List</h1>
+    </div>
+  )
+}
